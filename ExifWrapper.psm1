bcca1c9bcbd9b99bf5d37dae5b03c8d12889ad4b
Jan Rydval <jan@rydv.al>
2020-02-25 11:10:30 +0100

    first commit

---
+++ ExifWrapper.psm1
@@ -0,0 +1,63 @@
+function Get-Exif {
+
+    [CmdletBinding(DefaultParameterSetName = "File")]   
+    param(
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ParameterSetName = "Path",
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Path to one or more JPG files.")]
+        [Alias("FileName")]
+        [ValidateNotNullOrEmpty()]
+        [string[]]
+        $Path,
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ParameterSetName = "File",
+            ValueFromPipeline = $true,
+            HelpMessage = "File Item or Items.")]
+        [Alias("FileItem")]
+        [ValidateNotNullOrEmpty()]
+        [System.IO.FileInfo[]]
+        $File
+    )
+
+    begin { 
+        Write-Debug "Get-Exif Begin"
+     }
+
+    end { 
+        Write-Debug "Get-Exif End"
+     }
+
+    process {
+        $paramSet = $PSCmdlet.ParameterSetName
+        Write-Debug "Get-Exif - ParamSet: $paramSet, Parameters - Path: $Path, File: $File"
+
+        if( $paramSet -eq "File" ) {
+            $Path = $File.FullName
+        } else {
+            $File = Get-Item $Path
+        }
+
+        Write-Debug "Get-Exif - after resolution - Path: $Path, File: $File"
+        
+        (exif -m $Path) | ForEach-Object {
+            $name, $value = $_ -split "`t"
+            $name = $name -replace '\s', '' -replace'[\(\)]', ''   
+
+            if($name.startsWith("DateandTime")) {
+                $value = Get-Date ($value -replace "^(.*?):(.*?):(.*?) ", "`$1.`$2.`$3 ")
+            }
+
+            Write-Debug "Get-Exif - Addind NoteProperty: $name = $value"
+            Add-Member -InputObject $File[0] -NotePropertyName $name -NotePropertyValue $value
+           }
+       $File
+    }
+
+}
+
+
+
---
+++ README.md
@@ -0,0 +1,28 @@
+# ExifWrapper-PowerShellModule
+
+Import module:
+    Import-Module ./ExifWrapper.psm1
+
+Use function Get-exif:
+    Get-Exif [-Path] "filename.jpg" | Select-Object *
+    Get-Exif [-File] (Get-Item filename.jpg) | Select-Object *
+    "filename.jpg" | Get-Exif | Select-Object *
+    Get-Item filename.jpg | Get-Exif | Select-Object *
+
+The returned FileInfo object contains all the exif properties:
+    Manufacturer               : Apple
+    Model                      : iPhone 11 Pro Max
+    X-Resolution               : 72
+    Y-Resolution               : 72
+    ResolutionUnit             : Inch
+    Software                   : 13.3.1
+    DateandTime                : 2/18/2020 4:57:02 PM
+    ExposureTime               : 1/799 sec.
+    F-Number                   : f/2.0
+    ExposureProgram            : Normal program
+    ISOSpeedRatings            : 20
+    ExifVersion                : Unknown Exif Version
+    DateandTimeOriginal        : 2/18/2020 4:57:02 PM
+    DateandTimeDigitized       : 2/18/2020 4:57:02 PM
+
+Dates are [datetime] objects.
