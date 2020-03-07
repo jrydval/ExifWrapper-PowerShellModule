58f706aa75eeb8d53831c8d09b7fe101ce00cebd
jrydval <jrydval@post.cz>
2020-03-07 08:10:22 +0100

    add test

--- tests/ExifWrapper.test.ps1
+++ tests/ExifWrapper.test.ps1
@@ -1,9 +1,20 @@
-. ../ExifWrapper.ps1
+Import-Module (Join-Path ".." "ExifWrapper.psm1") -Force
 
 Describe -Name FirstTest {
+
+    Mock -CommandName exif -MockWith { 
+        "DateandTimeOriginal`t2020:01:02 01:02:03", 
+        "Apperture`t2.0" 
+    }
+    Mock -CommandName Get-Item -MockWith { 
+        New-Object [PSCustomObject]
+    }
+
+
     Context "Have fun" {
         It "Should work" {
-            $true | Should -Be $true
+            $fe = Get-Exif "IMG.jpg" 
+            $fe | Should -HaveCount 2
         }
     }
 
