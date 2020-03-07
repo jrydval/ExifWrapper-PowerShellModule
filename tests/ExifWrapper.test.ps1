3b1a4c9222481e448b883794081de8962d13b250
Jan Rydval <jan@rydv.al>
2020-03-07 17:56:21 +0100

    correct test script

--- tests/ExifWrapper.test.ps1
+++ tests/ExifWrapper.test.ps1
@@ -1,20 +1,27 @@
 Import-Module (Join-Path ".." "ExifWrapper.psm1") -Force
 
-Describe -Name FirstTest {
+Describe -Name "Attribute Tests" {
 
-    Mock -CommandName exif -MockWith { 
+    Mock -CommandName exif -ModuleName ExifWrapper -MockWith { 
         "DateandTimeOriginal`t2020:01:02 01:02:03", 
         "Apperture`t2.0" 
     }
-    Mock -CommandName Get-Item -MockWith { 
-        New-Object [PSCustomObject]
+    
+    Mock -CommandName Get-Item -ModuleName ExifWrapper -MockWith { 
+        [System.IO.FileInfo]::new("img.jpg")
     }
 
 
-    Context "Have fun" {
-        It "Should work" {
-            $fe = Get-Exif "IMG.jpg" 
+    Context "With 2 attributes" {
+        $fe = Get-Exif "IMG.jpg" 
+        
+        It "Has two attribites" {
+
             $fe | Should -HaveCount 2
+        }
+
+        It "Has date as datetime type" {
+            $fe.DateandTimeOriginal | Should -HaveType [datetime]
         }
     }
 
